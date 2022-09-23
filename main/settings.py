69a1bb9021f100a1b8acfaf22d7ef15a029ed080
mehmet-turkeri <mehmetturkeri51@gmail.com>
2022-09-23 15:07:51 +0200

    up

--- main/settings.py
+++ main/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media_root"
